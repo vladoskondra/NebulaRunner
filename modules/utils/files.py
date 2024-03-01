62b676b343bfbff9775b01c9d2654c81f138a1ab
vladoskondra <shermanwaydesign@gmail.com>
2024-03-01 19:00:02 +0300

    NebulaRunner v.2.1

--- modules/utils/files.py
+++ modules/utils/files.py
@@ -1,12 +1,15 @@
 import json
+from pathlib import Path
+
+root_dir = Path(__file__).parent.parent.parent
 
 
 def read_file(file):
     file_name = ''
     if file == 'hero':
-        file_name = './configs/my_hero.json'
+        file_name = f'{root_dir}/configs/my_hero.json'
     if file == 'config':
-        file_name = './configs/tg_API.json'
+        file_name = f'{root_dir}/configs/tg_API.json'
     with open(file_name, encoding='utf-8') as json_file:
         loaded_json = json.load(json_file)
     return loaded_json
@@ -16,8 +19,8 @@ def update_file(dict_to_push):
     dict_cat = f"{dict_to_push=}".split("=")[0]
     file_name = ''
     if dict_cat == 'hero':
-        file_name = './configs/my_hero.json'
+        file_name = f'{root_dir}/configs/my_hero.json'
     if dict_cat == 'config':
-        file_name = './configs/tg_API.json'
+        file_name = f'{root_dir}/configs/tg_API.json'
     with open(file_name, 'w', encoding='utf-8') as outfile:
         json.dump(dict_to_push, outfile, ensure_ascii=False, indent=2)
