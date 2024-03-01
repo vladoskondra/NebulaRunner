6725a31145bfe4cd6c09a41ef456bd19cbeb5d24
vladoskondra <shermanwaydesign@gmail.com>
2024-03-01 21:20:52 +0300

    NebulaRunner v.2.1

--- modules/utils/files.py
+++ modules/utils/files.py
@@ -1,5 +1,6 @@
 import json
 from pathlib import Path
+import os
 
 root_dir = Path(__file__).parent.parent.parent
 
@@ -7,9 +8,9 @@ root_dir = Path(__file__).parent.parent.parent
 def read_file(file):
     file_name = ''
     if file == 'hero':
-        file_name = f'{root_dir}\\configs\\my_hero.json'
+        file_name = os.path.join(root_dir, 'configs', 'my_hero.json')
     if file == 'config':
-        file_name = f'{root_dir}\\configs\\tg_API.json'
+        file_name = os.path.join(root_dir, 'configs', 'tg_API.json')
     with open(file_name, encoding='utf-8') as json_file:
         loaded_json = json.load(json_file)
     return loaded_json
@@ -19,9 +20,9 @@ def update_file(dict_to_push):
     dict_cat = f"{dict_to_push=}".split("=")[0]
     file_name = ''
     if dict_cat == 'hero':
-        file_name = f'{root_dir}\\configs\\my_hero.json'
+        file_name = os.path.join(root_dir, 'configs', 'my_hero.json')
     if dict_cat == 'config':
-        file_name = f'{root_dir}\\configs\\tg_API.json'
+        file_name = os.path.join(root_dir, 'configs', 'tg_API.json')
     print(root_dir)
     # print(file_name)
     with open(file_name, 'w', encoding='utf-8') as outfile:
