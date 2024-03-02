d951c87461e9bf576040846aa2ec0aef50d59a95
vladoskondra <shermanwaydesign@gmail.com>
2024-03-02 18:12:06 +0300

    fixing up to date

--- modules/starter/starter.py
+++ modules/starter/starter.py
@@ -3,14 +3,13 @@ from telethon import TelegramClient
 from modules.utils.files import *
 
 
-repo = git.Repo("./.git", search_parent_directories=True)
-status = repo.git.status()
-print(status)
-for x in repo.index.diff("HEAD"):
-    # Just print
-    print(x)
-if "Your branch is up to date with 'origin/master'" in str(status):
-    print('UPDATED')
+# repo = git.Repo("./.git", search_parent_directories=True)
+# status = repo.git.status()
+# print(status)
+# for x in repo.index.diff("HEAD"):
+#     # Just print
+#     print(x)
+print('UPDATED')
 # if len(diff) != 0:
 #     print('Обнаружена новая версия скрипта, обновляю!')
 # git.Repo('.git').remote().pull()
