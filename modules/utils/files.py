f927ee123269bc329ecb00cd7c25925bdd4da5f5
vladoskondra <shermanwaydesign@gmail.com>
2024-03-06 19:39:35 +0300

    NebulaRunner v2.1

--- modules/utils/files.py
+++ modules/utils/files.py
@@ -6,12 +6,52 @@ root_dir = Path(__file__).parent.parent.parent
 
 def read_file(file):
     file_name = ''
+    raw = {}
     if file == 'hero':
         file_name = f'{root_dir}/configs/my_hero.json'
+        raw = {
+            "name": "",
+            "cur_hp": 0,
+            "max_hp": 0,
+            "lvl": 0,
+            "energy": 5,
+            "state": "none",
+            "captcha": False,
+            "mode": "stop",
+            "intox": False,
+            "target": "",
+            "mob_lvl": 1,
+            "mob_cls": "any",
+            "farm_loc": "",
+            "loc": "default",
+            "prof": "none",
+            "prof_loc": "",
+            "edem": False,
+            "multitool": False,
+            "cosmos": False,
+            "stats": {
+                "class": "none",
+                "atk": 1,
+                "def": 1,
+                "ddg": 0,
+                "crit": 0,
+                "acc": 0,
+                "spd": 0
+            }
+        }
     if file == 'config':
         file_name = f'{root_dir}/configs/tg_API.json'
-    with open(file_name, encoding='utf-8') as json_file:
-        loaded_json = json.load(json_file)
+        raw = {
+            "api_id": 0,
+            "api_hash": "",
+            "password": "-"
+        }
+    if Path(file_name).is_file():
+        with open(file_name, encoding='utf-8') as json_file:
+            loaded_json = json.load(json_file)
+    else:
+        loaded_json = raw
+        update_file(file, raw)
     return loaded_json
 
 
