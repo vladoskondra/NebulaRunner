fc7ff07a49769431e4f80fcf041f688784fde7e9
vladoskondra <shermanwaydesign@gmail.com>
2024-04-01 14:51:41 +0300

    NebulaRunner v2.2.3

--- modules/game/fight_sim.py
+++ modules/game/fight_sim.py
@@ -25,76 +25,89 @@ async def fight_simulation(optional_mob=0):
         'speed': hero['hero']['spd']
     }
     mob_cls = 'any'
-    if hero["farm_cfg"]['mob_cls'] != 'any':
-        mob_cls = hero["farm_cfg"]['mob_cls']
-    if optional_mob == 0:
-        mob = get_mob(hero["farm_cfg"]['mob_lvl'], cls=mob_cls)
+    mobs_list = []
+    if hero['space']['cosmos']:
+        for pot_cls in ['warrior', 'ranger', 'mage']:
+            if optional_mob == 0:
+                mob = get_mob(hero["farm_cfg"]['mob_lvl'], cls=pot_cls)
+            else:
+                mob = get_mob(optional_mob, cls=pot_cls)
+            mobs_list.append(mob)
     else:
-        mob = get_mob(optional_mob, cls=mob_cls)
-    enemy_obj = {
-        'name': 'mob',
-        'class': mob['monsterType'].lower(),
-        'lvl': mob['level'],
-        'hps': mob['hP'],
-        'hpl': mob['hP'],
-        'dmg': round(mob['attack']),
-        'def': round(mob['def']),
-        'ddg': mob['dodge'],
-        'crit': mob['critRate'],
-        'acc': mob['accuracy'],
-        'speed': mob['speed']
-    }
-    wins = {
-        player_obj['name']: 0,
-        enemy_obj['name']: 0
-    }
-    loops = 10000
-    fight_loop = 0
-    while fight_loop < loops:
-        fight_text = ''
-        first = player_obj
-        second = enemy_obj
-        if player_obj['speed'] < enemy_obj['speed']:
-            first = enemy_obj
-            second = player_obj
-        elif player_obj['speed'] == enemy_obj['speed']:
-            rnd = round(random.uniform(0, 1))
-            if rnd == 1:
+        if hero["farm_cfg"]['mob_cls'] != 'any':
+            mob_cls = hero["farm_cfg"]['mob_cls']
+        if optional_mob == 0:
+            mob = get_mob(hero["farm_cfg"]['mob_lvl'], cls=mob_cls)
+        else:
+            mob = get_mob(optional_mob, cls=mob_cls)
+        mobs_list.append(mob)
+    wins_list = []
+    for m in mobs_list:
+        enemy_obj = {
+            'name': 'mob',
+            'class': m['monsterType'].lower(),
+            'lvl': m['level'],
+            'hps': m['hP'],
+            'hpl': m['hP'],
+            'dmg': round(m['attack']),
+            'def': round(m['def']),
+            'ddg': m['dodge'],
+            'crit': m['critRate'],
+            'acc': m['accuracy'],
+            'speed': m['speed']
+        }
+        wins = {
+            player_obj['name']: 0,
+            enemy_obj['name']: 0
+        }
+        loops = 10000
+        fight_loop = 0
+        while fight_loop < loops:
+            fight_text = ''
+            first = player_obj
+            second = enemy_obj
+            if player_obj['speed'] < enemy_obj['speed']:
                 first = enemy_obj
                 second = player_obj
-        rounds = 0
-        dodges = {
-            first['name']: 0,
-            second['name']: 0
-        }
-        while first['hpl'] > 0 and second['hpl'] > 0:
-            randomDDG_2 = random.uniform(0, 100)
-            if randomDDG_2 <= (second['ddg'] - first['acc']):
-                dodges[second['name']] += 1
-            else:
-                randomCRIT_1 = random.uniform(0, 100)
-                if randomCRIT_1 <= first['crit']:
-                    DD = round((first['dmg'] - second['def']) * 1.2)
-                    minDD = round(second['hps'] * MIN_DD_CLASS[second['class']].value)
-                    if DD <= minDD:
-                        DD = minDD
-                    second['hpl'] = second['hpl'] - DD
+            elif player_obj['speed'] == enemy_obj['speed']:
+                rnd = round(random.uniform(0, 1))
+                if rnd == 1:
+                    first = enemy_obj
+                    second = player_obj
+            rounds = 0
+            dodges = {
+                first['name']: 0,
+                second['name']: 0
+            }
+            while first['hpl'] > 0 and second['hpl'] > 0:
+                randomDDG_2 = random.uniform(0, 100)
+                if randomDDG_2 <= (second['ddg'] - first['acc']):
+                    dodges[second['name']] += 1
                 else:
-                    DD = first['dmg'] - second['def']
-                    minDD = round(second['hps'] * MIN_DD_CLASS[second['class']].value)
-                    if DD <= minDD:
-                        DD = minDD
-                    second['hpl'] = second['hpl'] - DD
-            tmpHero = first
-            first = second
-            second = tmpHero
-            rounds += 1
-        fight_loop += 1
-        if second['hpl'] < 0:
-            wins[first['name']] += 1
-        else:
-            wins[second['name']] += 1
-        first['hpl'] = first['hps']
-        second['hpl'] = second['hps']
-    winRate = float('{:.2f}'.format((wins['hero'] / fight_loop) * 100))
-    return winRate
+                    randomCRIT_1 = random.uniform(0, 100)
+                    if randomCRIT_1 <= first['crit']:
+                        DD = round((first['dmg'] - second['def']) * 1.2)
+                        minDD = round(second['hps'] * MIN_DD_CLASS[second['class']].value)
+                        if DD <= minDD:
+                            DD = minDD
+                        second['hpl'] = second['hpl'] - DD
+                    else:
+                        DD = first['dmg'] - second['def']
+                        minDD = round(second['hps'] * MIN_DD_CLASS[second['class']].value)
+                        if DD <= minDD:
+                            DD = minDD
+                        second['hpl'] = second['hpl'] - DD
+                tmpHero = first
+                first = second
+                second = tmpHero
+                rounds += 1
+            fight_loop += 1
+            if second['hpl'] < 0:
+                wins[first['name']] += 1
+            else:
+                wins[second['name']] += 1
+            first['hpl'] = first['hps']
+            second['hpl'] = second['hps']
+        winRate = float('{:.2f}'.format((wins['hero'] / fight_loop) * 100))
+        wins_list.append(winRate)
+    return min(wins_list)
